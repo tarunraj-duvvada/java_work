6994221bb18439ed436bfde322f57765f834cd60
tarunraj-duvvada <rajduvvada@gmail.com>
2025-06-23 20:29:06 +0530

    second/studentPerformance.java

---
+++ second/studentLms_6.java
@@ -0,0 +1,18 @@
+package second;
+
+public class studentLms_6 {
+     int student_Id ;
+     String studentName;
+     float attendence;
+     short performance_scores;
+     float trainer_ratings;
+    public void Display_student_details() {
+         System.out.println("Student ID = "+ student_Id);
+         System.out.println("Student Name = "+ studentName);
+         System.out.println("Student Attendence = "+ attendence);
+         System.out.println("Student performance scores = "+ performance_scores);
+         System.out.println("Student trainer rating = "+ trainer_ratings);
+
+    }
+}
+
---
+++ second/studentPerformance.java
@@ -0,0 +1,13 @@
+package second;
+
+import java.util.Scanner;
+
+public class studentPerformance{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+
+}
+    
+
